4e379161c47e5bdfb11da5a5a7ee48338cbeb364
zlmalu <39517236+zlmalu@users.noreply.github.com>
2022-04-18 08:51:05 +0800

    1

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -29,7 +29,10 @@ class Index
     {
         # html路径: ../view/index.html
         // return response(file_get_contents(dirname(dirname(__FILE__)).'/view/index.html'));
-        echo 11222;
+        // $url='https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code';
+     // $html = file_get_contents($url);
+     // echo $html;
+        echo Request::get('code');
     }
 
 
